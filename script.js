4e19c3b22ccec3c420497c8c5cc1b66fbb9897a3
lloret82 <adbmanchester@gmail.com>
2021-03-03 12:33:20 +0000

    added comments to js file

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Assignment Code
-let generateBtn = document.querySelector("#generate");
+//created variable containing all the characters, a variable for the password string and one for the generated password
 function generatePassword() {
   let lowerCase = "abcdefghijklmnopqrstuvwxyz";
   let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -7,7 +6,9 @@ function generatePassword() {
   let special = "!@£$%^&*()_+={}[]:|;?><,./~`";
   let finalPasswordCharacterOptions = "";
   let finalPassword = "";
+  // added a parseInt function to transform the user input string and parse into an integer
   let length = parseInt(prompt("How long would you like your password, between 8 and 128"))
+  // added a validation for the user input, lenght and isNaN so the user can't input not number characters and lenght outside the parameters
   if (isNaN(length)) {
     alert("Please insert a valid Number!")
     return;
@@ -20,10 +21,12 @@ function generatePassword() {
     alert("the password need to be a minimum of 8 characters");
     return;
   }
+  // declaring boolean variables based on user input
   let wantsLowerCase = confirm("Do you want lower case letters?");
   let wantsUpperCase = confirm("Do you want upper case letters?");
   let wantsNumber = confirm("Do you want Number?");
   let wantsSpecial = confirm("Do you want Special characters?");
+  // added a while loop to validate that at least one option is selected
   while (wantsLowerCase == false && wantsUpperCase == false && wantsNumber == false && wantsSpecial == false) {
     alert("Please select one type of character");
     wantsLowerCase = confirm("Do you want lower case letters?");
@@ -31,6 +34,7 @@ function generatePassword() {
     wantsNumber = confirm("Do you want Number?");
     wantsSpecial = confirm("Do you want Special characters?");
   }
+  // if a character type is selected, it will be added to the variable of final password options
   if (wantsLowerCase === true) {
 
     finalPasswordCharacterOptions += lowerCase;
@@ -45,7 +49,7 @@ function generatePassword() {
     finalPasswordCharacterOptions += special;
   }
 
-
+// a loop run for the password lenght choose by the user (-1 to return the exact lenght)
   for (let i = 0; i <= length - 1; i++) {
     let randomCharacter = Math.floor(Math.random() * finalPasswordCharacterOptions.length)
     finalPassword += finalPasswordCharacterOptions[randomCharacter]
@@ -57,6 +61,7 @@ function generatePassword() {
 
 
 // Write password to the #password input
+
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
@@ -66,4 +71,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+let generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
